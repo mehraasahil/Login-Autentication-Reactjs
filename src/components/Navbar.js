a253180ec26a1b18fd160430c4c75118d2265ae4
mehraasahil <mehra3249@gmail.com>
2023-02-13 01:35:21 +0530

    added

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+      <span className='logo'>Sahil App</span>
+      <ul className='list'>
+        <li className='listitem'>
+            <img src="" alt="" className='avatar' />
+        </li>
+        <li className='listitem'>Sahil Mehra</li>
+        <li className="listitem">Logoutbutton</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
